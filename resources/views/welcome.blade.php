a4e704a63f38e53d301426c9e1b69774e9007042
zebinzb <1051177285@qq.com>
2019-03-07 15:00:37 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 66px;
+            }
+        </style>
+    </head>
+    <body>
+    <div class="container">
+        <div class="content">
+            <div class="title">Hello Laravel! - by LiuZeBin</div>
+        </div>
+    </div>
+    </body>
+</html>
